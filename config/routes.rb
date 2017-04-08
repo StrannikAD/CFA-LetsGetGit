19c636ae6116ab76049f26550f23221b3e8c87b3
Shouhei Yamauchi <shouhei.yamauchi@live.com>
2017-04-09 09:43:09 +1000

    updated quizzes and results page

--- config/routes.rb
+++ config/routes.rb
@@ -11,9 +11,11 @@ Rails.application.routes.draw do
 
   get 'pages/results'
 
+  get 'pages/quiz_submission'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" } 
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   #   delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
   # end
 end
